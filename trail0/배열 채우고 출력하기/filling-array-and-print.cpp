b7712502ac6980af158cb88c49492a786f71683d
eunho0216 <sign020216@gmail.com>
2026-07-21 17:34:18 +0900

    [Passed] 배열 채우고 출력하기 (2ms, 0MB)

---
+++ trail0/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char array[10];
+    for(int i = 0 ; i<10; i++){
+        cin >> array[i];
+    }
+    for(int i = 9; i >=0; i--){
+        cout << array[i];
+    }
+    return 0;
+}
